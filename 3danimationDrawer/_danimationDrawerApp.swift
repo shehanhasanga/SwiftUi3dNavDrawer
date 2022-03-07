eef977de15a2fbd9fd25b509c4845441f2549867
shehanhasanga <shehanhasanga.15@cse.mrt.ac.lk>
2022-03-07 17:39:36 +0530

    Initial Commit

---
+++ 3danimationDrawer/_danimationDrawerApp.swift
@@ -0,0 +1,17 @@
+//
+//  _danimationDrawerApp.swift
+//  3danimationDrawer
+//
+//  Created by shehan karunarathna on 2022-03-07.
+//
+
+import SwiftUI
+
+@main
+struct _danimationDrawerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
